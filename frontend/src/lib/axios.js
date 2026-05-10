12f15e1a223d566a8be667dd29e98d49fff4ac79
Koushik Ahmed <koushik101517@gmail.com>
2026-05-10 11:55:47 +0600

    backend deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,8 +4,8 @@ import axios from "axios";
 // const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
-    // baseURL: API_URL,
+  baseURL: "https://chatbot-mmwj.onrender.com",
+  // baseURL: API_URL,
   withCredentials: true, // This is IMPORTANT for cookies
   headers: {
     "Content-Type": "application/json",
@@ -18,5 +18,5 @@ axiosInstance.interceptors.response.use(
   (error) => {
     console.error("API Error:", error.response?.data || error.message);
     return Promise.reject(error);
-  }
+  },
 );
